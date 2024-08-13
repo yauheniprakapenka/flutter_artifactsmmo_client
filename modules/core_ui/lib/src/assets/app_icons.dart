ae9272b13f4d2ff16c94762e0ccbf094ada8c40a
yauheni.prakapenka <>
2024-08-13 09:01:02 +0300

    add assets

--- modules/core_ui/lib/src/assets/app_icons.dart
+++ modules/core_ui/lib/src/assets/app_icons.dart
@@ -1,7 +1,12 @@
 class AppIcons {
+  static const String automation = 'assets/icons/automation.svg';
   static const String bank = 'assets/icons/bank.svg';
+  static const String buy = 'assets/icons/buy.svg';
   static const String clock = 'assets/icons/clock.svg';
+  static const String complete = 'assets/icons/complete.svg';
   static const String crafting = 'assets/icons/crafting.svg';
+  static const String deposit = 'assets/icons/deposit.svg';
+  static const String equip = 'assets/icons/equip.svg';
   static const String exchange = 'assets/icons/exchange.svg';
   static const String fight = 'assets/icons/fight.svg';
   static const String focus = 'assets/icons/focus.svg';
@@ -9,5 +14,9 @@ class AppIcons {
   static const String grid = 'assets/icons/grid.svg';
   static const String info = 'assets/icons/info.svg';
   static const String move = 'assets/icons/move.svg';
+  static const String moves = 'assets/icons/moves.svg';
   static const String question = 'assets/icons/question.svg';
+  static const String sell = 'assets/icons/sell.svg';
+  static const String task = 'assets/icons/task.svg';
+  static const String unequip = 'assets/icons/unequip.svg';
 }
